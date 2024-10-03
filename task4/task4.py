2c344f032be29fa4769444ee4eeab24b7799a033
TyphoonTH <60212124+TyphoonTH@users.noreply.github.com>
2024-10-03 21:50:16 +0300

    Add files via upload

--- task4/task4.py
+++ task4/task4.py
@@ -1,1 +1,12 @@
-
+nums=[]
+with open("numbers.txt","r") as numbers:
+    for line in numbers:
+        nums.append(int(line))
+sum=0 #сумма всех значений
+res=0 #полученная сумма шагов
+for i in nums:
+    sum+=i
+goal=round(sum/len(nums))
+for i in nums:
+    res+=(abs(goal-i))
+print(res)    
