f263dccbea12178f1f405accd7e1a9c06c664dc9
TyphoonTH <60212124+TyphoonTH@users.noreply.github.com>
2024-10-03 21:39:31 +0300

    Add files via upload

--- task2/task2.py
+++ task2/task2.py
@@ -1,1 +1,18 @@
 
+with open("circle.txt","r") as circle:
+    circle_data=circle.read().split('\n')
+    
+center, radius=[float(i) for i in circle_data[0].split()],float(circle_data[1])
+
+with open("dot.txt","r") as points:
+    for line in points:
+        point=[float(i) for i  in line.split()]
+        if (point[0]-center[0])**2+(point[1]-center[1])**2<radius**2:
+            print(1)
+        elif (point[0]-center[0])**2+(point[1]-center[1])**2==radius**2:
+            print(0)   
+        else:
+            print(2) 
+
+
+   
