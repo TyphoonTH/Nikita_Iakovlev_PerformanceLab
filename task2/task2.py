8d7649eadc8090b8fe6d8247c2fee7a9ca4362e7
TyphoonTH <60212124+TyphoonTH@users.noreply.github.com>
2024-10-10 14:33:25 +0300

    Update task2.py

--- task2/task2.py
+++ task2/task2.py
@@ -1,10 +1,11 @@
+circle_file, points_file=input(), input()
 
-with open("circle.txt","r") as circle:
+with open(circle_file,"r") as circle:
     circle_data=circle.read().split('\n')
     
 center, radius=[float(i) for i in circle_data[0].split()],float(circle_data[1])
 
-with open("dot.txt","r") as points:
+with open(points_file,"r") as points:
     for line in points:
         point=[float(i) for i  in line.split()]
         if (point[0]-center[0])**2+(point[1]-center[1])**2<radius**2:
@@ -12,7 +13,4 @@ with open("dot.txt","r") as points:
         elif (point[0]-center[0])**2+(point[1]-center[1])**2==radius**2:
             print(0)   
         else:
-            print(2) 
-
-
-   
+            print(2)    
