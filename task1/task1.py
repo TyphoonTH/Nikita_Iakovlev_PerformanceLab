d9866635c6fe796fdaeebaa837caa64565acf062
TyphoonTH <60212124+TyphoonTH@users.noreply.github.com>
2024-10-10 18:48:31 +0300

    Update task1.py

--- task1/task1.py
+++ task1/task1.py
@@ -1,12 +1,19 @@
 import sys
 
 n,m=int(sys.argv[1]), int(sys.argv[2])
-loop=[i for i in range(1, n+1)] #круговой массив
-final=[1] #массив для хранения последних чисел
-last_num=m-1 #последнее число массива
-while last_num!=0:
-    final.append(loop[last_num])
-    last_num=last_num+m-1
-    if last_num>=n-1:
-        last_num-=n
-print(''.join([str(x) for x in final]))   
+if n>0 or m>0:  
+    loop=[1]#массив для чисел
+    last_num=0 #последнее число массива
+    final=[]
+    template=[i for i in range(1,n+1)] #массив значений числа
+    while last_num!=1:
+        for i in range(0,m-1):
+            loop.append(loop[i]+1 if loop[i]<=n-1 else loop[i]-n+1)
+        last_num=loop[len(loop)-1]
+        final.append(loop[0])
+        loop=[last_num]
+        
+    print(''.join([str(x) for x in final])) 
+        
+else:
+    print('Введите положительные числа')
